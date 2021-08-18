c37c9e0b3ef586d3d66043ffbf6154c1259547c1
Nannyju <88876387+Nannyju@users.noreply.github.com>
2021-08-18 18:53:17 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,11 +1,15 @@
 <h1> Julaluck Noosuwan 62101639 </h1>
 
-<iframe src="https://thingspeak.com/channels/1479261/charts/1?bgcolor=%23ffffff&color=%23d62020&dynamic=true&results=60&type=line&update=15 style="width:500px;height:600px;"></iframe>
+<br></br>
 
+<iframe src="https://thingspeak.com/channels/1479261/charts/1?bgcolor=%23ffffff&color=%23d62020&dynamic=true&results=60&type=line&update=15"></iframe>
+
+<br></br>
 <br></br>
 
 <iframe src="https://thingspeak.com/channels/1479261/charts/2?bgcolor=%23ffffff&color=%23d62020&dynamic=true&results=60&type=line&update=15"></iframe>
 
+<br></br>
 <br></br>
 
 <iframe src="https://thingspeak.com/channels/1479261/charts/3?bgcolor=%23ffffff&color=%23d62020&dynamic=true&results=60&type=line&update=15"></iframe>

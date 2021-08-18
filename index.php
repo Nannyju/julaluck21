c3dbe9097150fa42f6b315f44ea749fb834b6f36
Nannyju <88876387+Nannyju@users.noreply.github.com>
2021-08-18 18:48:03 +0700

    Update index.php

--- index.php
+++ index.php
@@ -2,4 +2,10 @@
 
 <iframe src="https://thingspeak.com/channels/1479261/charts/1?bgcolor=%23ffffff&color=%23d62020&dynamic=true&results=60&type=line&update=15"></iframe>
 
+<br></br>
+
 <iframe src="https://thingspeak.com/channels/1479261/charts/2?bgcolor=%23ffffff&color=%23d62020&dynamic=true&results=60&type=line&update=15"></iframe>
+
+<br></br>
+
+<iframe src="https://thingspeak.com/channels/1479261/charts/3?bgcolor=%23ffffff&color=%23d62020&dynamic=true&results=60&type=line&update=15"></iframe>

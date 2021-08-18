0d6d0377ea3931934fbbbae6af140ce6aa71c306
Nannyju <88876387+Nannyju@users.noreply.github.com>
2021-08-18 21:20:20 +0700

    Update index.php

--- index.php
+++ index.php
@@ -2,15 +2,20 @@
 
 <br></br>
 
-<iframe src="https://thingspeak.com/channels/1479261/charts/1?bgcolor=%23ffffff&color=%23d62020&dynamic=true&results=60&type=line&update=15"></iframe>
-
-<br></br>
-<br></br>
-
-<iframe src="https://thingspeak.com/channels/1479261/maps/channel_show"></iframe>
-
-<br></br>
+<h2>Humidity</h2>
+<iframe src="https://thingspeak.com/channels/1479261/charts/1?bgcolor=%23ffffff&color=%23d62020&dynamic=true&results=60&type=line&update=15" 
+        style="border:none; height="300px" width="500px></iframe>
+        
 <br></br>
 
+<h2>Temperature</h2>
+<iframe src="https://thingspeak.com/channels/1479261/charts/2?bgcolor=%23ffffff&color=%23d62020&dynamic=true&results=60&type=line&update=15"
+        style="border:none; height="300px" width="500px></iframe>
+
+<br></br>
+
+<h2>Map</h2>
+        <iframe src="https://thingspeak.com/channels/1479261/maps/channel_show"
+        style="border:none; height="300px" width="500px></iframe>
 <br></br>
 <br></br>
